99db48d9b1ddeae65f8b4af405c11081ad25d41e
wildgrin <anshul.rai994@gmail.com>
2024-10-05 09:50:49 +0530

    dtUser creations along with save button click functionality

--- Pet Shop Management/UserModule.cs
+++ Pet Shop Management/UserModule.cs
@@ -7,18 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Pet_Shop_Management
 {
     public partial class UserModule : Form
     {
+        SqlConnection cn = new SqlConnection();
+        SqlCommand cm = new SqlCommand();
+        dbConnect dbcon = new dbConnect();
+        string title = "Pet Shop Management";
+
         public UserModule()
         {
             InitializeComponent();
+            cn = new SqlConnection(dbcon.connection());
         }
 
         private void btnSave_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (MessageBox.Show("Are you sure you want to register this user?", "User Registration", MessageBoxButtons.YesNo, MessageBoxIcon.Question )== DialogResult.Yes) ;
+                cm = new SqlCommand("INSERT INTO tbUser(name, address, phone, role, dob, password) VALUES(@name, @address, @phone, @role, @dob, @password)", cn);
+                cm.Parameters.AddWithValue("@name", txtName.Text);
+                cm.Parameters.AddWithValue("@address", txtAddress.Text);
+                cm.Parameters.AddWithValue("@phone", txtPhone.Text);
+                cm.Parameters.AddWithValue("@role", cbRole.Text);
+                cm.Parameters.AddWithValue("@dob", dtDob.Value);
+                cm.Parameters.AddWithValue("@password", txtPass.Text);
+
+                cn.Open();
+                cm.ExecuteNonQuery();
+                cn.Close();
+                MessageBox.Show("User has been registered successfully!", title);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,title);
+            }
 
         }
 
---
+++ Pet Shop Management/dbConnect.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Pet_Shop_Management
+{
+    class dbConnect
+    {
+        SqlConnection cn = new SqlConnection();
+        SqlCommand cm = new SqlCommand();
+        private string con;
+
+
+        public string connection()
+        {
+
+            con = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\anshu\source\repos\Pet Shop Management\Pet Shop Management\dbPetShop.mdf"";Integrated Security=True";
+            return con;
+        }
+
+        public void executeQuery(string sql)
+        {
+            try
+            {
+                cn.ConnectionString = connection();
+                cn.Open();
+                cm = new SqlCommand(sql, cn);
+                cm.ExecuteNonQuery();
+                cn.Close();
+
+            }
+            catch (Exception ex)
+            {
+                cn.Close();
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
